0a35e6144fb44cd88e76dc826228d853132ae371
Phalentin <63906585+Phalentin@users.noreply.github.com>
2024-10-01 18:03:48 +0200

    Added APB Website

--- README.md
+++
@@ -1,2 +0,0 @@
-# phalentin.github.io
-The licenses to the different projects on this site are linked to on their respective repositories, which are linked to on the website of the project.
--- anki-alg-set-generator-js/js/AlgCard.js
+++
@@ -1,9 +0,0 @@
-export class AlgCard {
-    constructor(name, alg, note, scramble, tags) {
-        this.name = name;
-        this.alg = alg;
-        this.note = note;
-        this.scramble = scramble;
-        this.tags = tags;
-    }
-}
